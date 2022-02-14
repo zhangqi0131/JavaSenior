cd7aa73c730512e049f30a465e162108c542eecb
zhangqi <chriz0131@163.com>
2022-02-14 14:14:51 +0800

    2-14

--- JDBC/src/com/zq/connection/ConnectionTest.java
+++ JDBC/src/com/zq/connection/ConnectionTest.java
@@ -2,6 +2,7 @@ package com.zq.connection;
 
 import org.junit.Test;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
@@ -12,7 +13,9 @@ import java.util.Properties;
  * 连接数据库的方式
  */
 public class ConnectionTest {
-    // 方法一
+    /**
+     * 方法一
+      */
     @Test
     public void testConnection1() throws SQLException {
         // 获取Driver实现类的对象
@@ -26,7 +29,9 @@ public class ConnectionTest {
         System.out.println(connection);
     }
 
-    // 方法二
+    /**
+     * 方法二
+      */
     @Test
     public void testConnection2() throws Exception {
         // 1.获取Driver实现类的对象:使用反射实现
@@ -46,7 +51,9 @@ public class ConnectionTest {
         System.out.println(connect);
     }
 
-    // 方式三：使用DriverManager替换Driver
+    /**
+     方式三：使用DriverManager替换Driver
+     */
     @Test
     public void testConnect3() throws Exception {
         // 1.获取Driver实现类对象
@@ -61,6 +68,51 @@ public class ConnectionTest {
         DriverManager.registerDriver(driver);
 
         // 4.获取连接
+        Connection connection = DriverManager.getConnection(url, user, password);
+        System.out.println(connection);
+    }
+
+    /**
+     * 方式四：
+      */
+    @Test
+    public void testConnection4() throws Exception {
+        // 1.获取连接的3个信息
+        String url = "jdbc:mysql://localhost:3306/test?useUnicode=true&charsetEncoding=utf-8";
+        String user = "root";
+        String password = "zq123";
+
+        // 2.获取Driver实现类对象
+        Class clazz = Class.forName("com.mysql.cj.jdbc.Driver");
+
+        /*Driver driver =(Driver) clazz.newInstance();
+
+        // 3.注册驱动
+        DriverManager.registerDriver(driver);*/
+
+        // 4.获取连接
+        Connection connection = DriverManager.getConnection(url, user, password);
+        System.out.println(connection);
+    }
+
+    /**
+     * 将数据库需要的4个基本信息声明在配置文件中，通过读取配置文件，获取连接
+     */
+    @Test
+    public void testConnection5() throws Exception {
+        // 1.读取配置文件中的4个基本信息
+        InputStream inputStream = ConnectionTest.class.getClassLoader().getResourceAsStream("jdbc.properties");
+        Properties pros = new Properties();
+        pros.load(inputStream);
+        String user = pros.getProperty("user");
+        String password = pros.getProperty("password");
+        String url = pros.getProperty("url");
+        String driverClass = pros.getProperty("driverClass");
+
+        // 2.加载驱动
+        Class.forName(driverClass);
+
+        // 3.获取连接
         Connection connection = DriverManager.getConnection(url, user, password);
         System.out.println(connection);
     }
