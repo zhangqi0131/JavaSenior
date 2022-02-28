51657c4076af199d4c081e05502bfab4e6384410
zhangqi <chriz0131@163.com>
2022-03-01 01:09:26 +0800

    servlet

---
+++ javaweb-demo/src/com/demo/servlets/AddServlet.java
@@ -0,0 +1,36 @@
+package com.demo.servlets;
+
+import com.demo.fruit.dao.FruitDAO;
+import com.demo.fruit.dao.impl.FruitDAOImpl;
+import com.demo.fruit.pojo.Fruit;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author ZhangQi
+ * @version 1.0.0
+ * @Description TODO
+ * @createTime 2022年03月01日 00:09  周二
+ */
+public class AddServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String fname = request.getParameter("fname");
+        String priceStr = request.getParameter("price");
+        int price = Integer.parseInt(priceStr);
+        String fcountStr = request.getParameter("fcount");
+        int fcount = Integer.parseInt(fcountStr);
+        String remark = request.getParameter("remark");
+
+        FruitDAO fruitDAO = new FruitDAOImpl();
+        boolean flag = fruitDAO.addFruit(new Fruit(0 , fname , price , fcount , remark));
+
+        System.out.println(flag ? "添加成功！" : "添加失败！");
+
+
+    }
+}
